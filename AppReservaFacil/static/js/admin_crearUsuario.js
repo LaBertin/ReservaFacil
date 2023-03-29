72f4af639a200a3a713b327b10172ce950f207f0
EnzoMenchise2525 <enzomenchise@gmail.com>
2023-03-28 21:39:37 -0300

    message (ctrl...)

--- AppReservaFacil/static/js/admin_crearUsuario.js
+++ AppReservaFacil/static/js/admin_crearUsuario.js
@@ -20,12 +20,13 @@ const validarFormulario = (e) => {
         case "nombreUser":
             validarCampo(expresiones.nombreUser, e.target, 'nombreUser');
             break;
-
         case "password":
-            validarCampo(expresiones.password, e.target, 'password');
+			validarCampo(expresiones.password, e.target, 'password');
+			validarPassword2();
+		    break;
+		case "password2":
+			validarPassword2();
             break;
-
-
     }
 }
 
@@ -47,6 +48,26 @@ const validarCampo = (expresion, input, campo) => {
     }
 }
 
+const validarPassword2 = () => {
+	const inputPassword1 = document.getElementById('password');
+	const inputPassword2 = document.getElementById('password2');
+
+	if(inputPassword1.value !== inputPassword2.value){
+		document.getElementById(`grupo__password2`).classList.add('formulario__grupo-incorrecto');
+		document.getElementById(`grupo__password2`).classList.remove('formulario__grupo-correcto');
+		document.querySelector(`#grupo__password2 i`).classList.add('fa-times-circle');
+		document.querySelector(`#grupo__password2 i`).classList.remove('fa-check-circle');
+		document.querySelector(`#grupo__password2 .formulario__input-error`).classList.add('formulario__input-error-activo');
+		campos['password'] = false;
+	} else {
+		document.getElementById(`grupo__password2`).classList.remove('formulario__grupo-incorrecto');
+		document.getElementById(`grupo__password2`).classList.add('formulario__grupo-correcto');
+		document.querySelector(`#grupo__password2 i`).classList.remove('fa-times-circle');
+		document.querySelector(`#grupo__password2 i`).classList.add('fa-check-circle');
+		document.querySelector(`#grupo__password2 .formulario__input-error`).classList.remove('formulario__input-error-activo');
+		campos['password'] = true;
+	}
+}
 
 
 inputs.forEach((input) => {
